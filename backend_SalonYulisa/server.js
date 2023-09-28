54a7c6f9256f4e1bc56608af38a29e120f2ab2d2
Oneyker21 <oneygaleano21@gmail.com>
2023-09-28 12:18:23 -0600

    alineamiento.

--- backend_SalonYulisa/server.js
+++ backend_SalonYulisa/server.js
@@ -1,26 +1,25 @@
 
-
 //Configuracion de la conexion a la base de datos
 
 
 const express = require('express');
- const mysql = require('mysql');
+const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
 const port = 5000;
 // Configuración de la conexión a la base de datos
 const db = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: 'Mysql2023', 
-database: 'world' 
-}); 
+    host: 'localhost',
+    user: 'root',
+    password: 'Mysql2023',
+    database: 'world'
+});
 db.connect((err) => {
-if (err) {
-console.error('Error de conexión a la base de datos:', err);
-} else {
-console.log('Conexión exitosa a la base de datos');
-}
+    if (err) {
+        console.error('Error de conexión a la base de datos:', err);
+    } else {
+        console.log('Conexión exitosa a la base de datos');
+    }
 });
 
 app.use(cors());
