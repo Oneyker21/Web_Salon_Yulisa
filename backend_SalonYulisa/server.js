a3a6f6e98c09fdd87efa988d7f3167acbdaf0a39
Oneyker21 <oneygaleano21@gmail.com>
2023-10-02 22:10:48 -0600

    coneccion resuelta.

--- backend_SalonYulisa/server.js
+++ backend_SalonYulisa/server.js
@@ -5,27 +5,35 @@ const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
 const port = 5000;
+
+app.use(express.json());
+
 // Configuración de la conexión a la base de datos
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Mysql2023',
-    database: 'world'
+    password: 'ONEYKER2105',
+    database: 'salon_yulisa'
 });
+
 db.connect((err) => {
     if (err) {
         console.error('Error de conexión a la base de datos:', err);
     } else {
-        console.log('Conexión exitosa a la base de datos');
+        console.error('Conexión exitosa a la base de datos');
     }
 });
 
 app.use(cors());
 
-// Iniciar el servidor 
+const crudRoutes = require('./routes/crudRoutes.js')(db); //Pasa la instancia de la base de datos a crudRoutes
+app.use('/crud', crudRoutes);
+
 app.listen(port, () => {
     console.log(`Servidor backend en funcionamiento en el puerto ${port}`);
 });
 
-const CrudRoutes = require('./routes/CrudRoutes')(db);
-app.use('/crud', CrudRoutes);
+
+
+
+
