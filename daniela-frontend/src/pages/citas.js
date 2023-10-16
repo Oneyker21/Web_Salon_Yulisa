2bf27ec9c7136ab3df5a1e7adcacefea63eb30ff
Daniela Baltodano <alejandrabaltodano67@gmail.com>
2023-10-16 13:23:07 -0600

    arreglo del form citas.

--- daniela-frontend/src/pages/citas.js
+++ daniela-frontend/src/pages/citas.js
@@ -3,7 +3,7 @@ import { Form, Row, Col, Container, FloatingLabel, Card, Button } from 'react-bo
 import Header from '../components/Header';
 import '../styles/App.css';
 
-function citas() {
+function Citas() {
 
   // Crear un estado para cada campo del formulario
   const [nombre, setNombre] = useState('');
@@ -128,4 +128,4 @@ function citas() {
   );
 }
 
-export default citas;
+export default Citas;
